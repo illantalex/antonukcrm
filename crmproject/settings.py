5e6499aa6366d5401b2a77aa220833407b3d05a1
Illya Antonuk <illantalex84@gmail.com>
2020-07-22 09:33:50 +0300

    Fixed keys import

--- crmproject/settings.py
+++ crmproject/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import dj_database_url
 import os
-from keys import *
+from crmproject.keys import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
