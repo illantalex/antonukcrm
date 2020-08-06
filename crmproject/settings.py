4fc41a4563677143458bc50c8900c5209c69e981
Illya Antonuk <illantalex84@gmail.com>
2020-08-06 22:05:37 +0300

    Experiment with MySQL

--- crmproject/settings.py
+++ crmproject/settings.py
@@ -110,6 +110,15 @@ DATABASES = {
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'OPTIONS': {
+#             'read_default_file': os.path.join(BASE_DIR, 'my.cnf'),
+#         },
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
